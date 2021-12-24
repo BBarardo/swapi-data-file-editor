f7e60217dcdddfc5980bb4b632adf2a4587e3c21
Bernardo Barardo <bernardo.s.barardo@gmail.com>
2021-12-24 15:38:13 +0000

    Changed data files to a more up-to-date version; Added code;

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log('Hello World');
+fs = require('fs');
+const SERVER_URL = 'STRING_TO_REPLACE';
+
+const data = require('./data');
+
+// console.log(data);
+
+for (var resource in data) {
+  if (Object.prototype.hasOwnProperty.call(data, resource)) {
+    console.log('-----------------------');
+    console.log('Doing -> ', resource);
+    console.log('-----------------------');
+
+    let newResource = [];
+
+    data[resource][resource].forEach(element => {
+      if (!resource.includes('films')) {
+        console.log(element?.name);
+
+        element.imageURL = `${SERVER_URL}/images/${resource}/${element?.name}.png`;
+        console.log(element);
+        newResource.push(element);
+      } else {
+        console.log(element?.title);
+
+        element.imageURL = `${SERVER_URL}/images/${resource}/${element?.title}.png`;
+        console.log(element);
+        newResource.push(element);
+      }
+    });
+
+    fs.writeFile(
+      `${resource}.josn`,
+      JSON.stringify(newResource),
+      function (err) {
+        if (err) return console.log(err);
+        console.log(`${resource} > ${resource}.josn`);
+      }
+    );
+  }
+}
+
+// fs.writeFile('helloworld.txt', JSON.stringify(data), function (err) {
+//   if (err) return console.log(err);
+//   console.log('Hello World > helloworld.txt');
+// });
