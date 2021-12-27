653a9ca827adb8e12b10d62ced3075a7d209c282
Bernardo Barardo <bernardo.s.barardo@gmail.com>
2021-12-27 11:31:40 +0000

    added edited tag on objects

--- index.js
+++ index.js
@@ -18,21 +18,72 @@ for (var resource in data) {
         console.log(element?.name);
 
         element.imageURL = `${SERVER_URL}/images/${resource}/${element?.name}.png`;
+        element.edited = new Date();
+        console.log(element);
+        newResource.push(element);
+      } else if (resource.includes('films')) {
+        console.log(element?.title);
+
+        element.imageURL = `${SERVER_URL}/images/${resource}/${element?.title}.png`;
+        element.edited = new Date();
+        console.log(element);
+        newResource.push(element);
+      } else if (resource.includes('people')) {
+        console.log(element?.name);
+        let name = '';
+
+        if (element?.name.includes('Ackbar')) {
+          name = 'Gial Ackbar';
+        } else {
+          name = element?.name;
+        }
+        element.imageURL = `${SERVER_URL}/images/${resource}/${name}.png`;
+        element.edited = new Date();
+        console.log(element);
+        newResource.push(element);
+      } else if (resource.includes('planets')) {
+        console.log(element?.name);
+        let name = '';
+
+        if (
+          element?.name.includes('Stewjon') ||
+          element?.name.includes('Tholoth') ||
+          element?.name.includes('Kalee')
+        ) {
+          name = null;
+        } else {
+          name = element?.name;
+        }
+        element.imageURL = `${SERVER_URL}/images/${resource}/${name}.png`;
+        element.edited = new Date();
         console.log(element);
         newResource.push(element);
       } else if (resource.includes('vehicles')) {
         console.log(element?.name);
 
-        element.imageURL = `${SERVER_URL}/images/${resource}/${element?.name.replace(
+        let name = '';
+
+        if (element?.name.includes('TIE bomber')) {
+          name = 'TIE/sa bomber';
+        } else if (element?.name.includes('AT-AT')) {
+          name = 'All Terrain Armored Transport';
+        } else if (element?.name.includes('Zephyr-G swoop bike')) {
+          name = 'Zephyr-G swoop';
+        } else if (element?.name.includes('Koro-2 Exodrive airspeeder')) {
+          name = 'Koro-2 all-environment exodrive airspeeder';
+        } else if (element?.name.includes('SPHA')) {
+          name = 'Self-Propelled Heavy Artillery';
+        } else if (element?.name.includes('AT-RT')) {
+          name = 'All Terrain Reconnaissance Transport';
+        } else {
+          name = element?.name;
+        }
+
+        element.imageURL = `${SERVER_URL}/images/${resource}/${name.replace(
           '/',
           '_'
         )}.png`;
-        console.log(element);
-        newResource.push(element);
-      } else {
-        console.log(element?.title);
-
-        element.imageURL = `${SERVER_URL}/images/${resource}/${element?.title}.png`;
+        element.edited = new Date();
         console.log(element);
         newResource.push(element);
       }
@@ -44,3 +95,5 @@ for (var resource in data) {
     console.log(`FINISH`);
   }
 }
+
+console.log(new Date());
